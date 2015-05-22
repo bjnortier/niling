fc0c066cdb60e6197592e2307e1fe9b86f2073ce
Ben Nortier <ben@bjnortier.com>
2015-05-22 11:57:19 +0200

    More SQLite functions

--- test/unit/stores.test.js
+++ test/unit/stores.test.js
@@ -44,59 +44,64 @@ describe('Stores', function() {
       function(cb) {
         store.getReference('abd', cb);
       },
-      // function(cb) {
-      //   store.updateReference('abd', {bar: []}, 0, 1, cb);
-      // },
-      // function(cb) {
-      //   store.getReference('abd', cb);
-      // },
+      function(cb) {
+        store.updateReference('abd', bson.serialize({bar: []}).bson, 0, 1, cb);
+      },
+      function(cb) {
+        store.getReference('abd', cb);
+      },
     ], function(err, result) {
       assert.isUndefined(err);
-      assert.equal(result.length, 2);
+      assert.equal(result.length, 4);
+
       assert.isUndefined(result[0]);
       assert.deepEqual(bson.deserialize(result[1][0]), {bar: [100]});
       assert.equal(result[1][1], 0);
+
+      assert.isUndefined(result[2]);
+      assert.deepEqual(bson.deserialize(result[3][0]), {bar: []});
+      assert.equal(result[3][1], 1);
       done();
     });
   });
 
-  // it('cannot put a reference when one exists', function(done) {
-  //   async.series([
-  //     function(cb) {
-  //       store.putReference('abd', {foo: [1,2,3]}, 0, cb);
-  //     },
-  //     function(cb) {
-  //       store.putReference('abd', {foo: [1,2,3]}, 0, cb);
-  //     },
-  //   ], function(err) {
-  //     assert.equal(err, 'reference with key "abd" exists');
-  //     done();
-  //   });
-  // });
+  it('cannot put a reference when one exists', function(done) {
+    async.series([
+      function(cb) {
+        store.putReference('abd', bson.serialize({foo: [1,2,3]}).bson, 0, cb);
+      },
+      function(cb) {
+        store.putReference('abd', bson.serialize({foo: []}).bson, 0, cb);
+      },
+    ], function(err) {
+      assert.equal(err, 'reference with key "abd" exists');
+      done();
+    });
+  });
 
-  // it('cannot update a non-existing reference', function(done) {
-  //   async.series([
-  //     function(cb) {
-  //       store.updateReference('abd', {bar: [4]}, 1, 2, cb);
-  //     },
-  //   ], function(err) {
-  //     assert.equal(err, 'reference "abd" doesn\'t exist');
-  //     done();
-  //   });
-  // });
+  it('cannot update a non-existing reference', function(done) {
+    async.series([
+      function(cb) {
+        store.updateReference('abd', bson.serialize({bar: [4]}).bson, 1, 2, cb);
+      },
+    ], function(err) {
+      assert.equal(err, 'reference "abd" doesn\'t exist');
+      done();
+    });
+  });
 
-  // it('must update a reference with the previous version', function(done) {
-  //   async.series([
-  //     function(cb) {
-  //       store.putReference('abd', {foo: [1,2,3]}, 0, cb);
-  //     },
-  //     function(cb) {
-  //       store.updateReference('abd', {bar: [4]}, 1, 2, cb);
-  //     },
-  //   ], function(err) {
-  //     assert.equal(err, 'wrong version for updating reference "abd"');
-  //     done();
-  //   });
-  // });
+  it('must update a reference with the previous version', function(done) {
+    async.series([
+      function(cb) {
+        store.putReference('abd', bson.serialize({foo: [1,2,3]}).bson, 0, cb);
+      },
+      function(cb) {
+        store.updateReference('abd', bson.serialize({bar: [4]}).bson, 1, 2, cb);
+      },
+    ], function(err) {
+      assert.equal(err, 'wrong version for updating reference "abd"');
+      done();
+    });
+  });
 
 });
